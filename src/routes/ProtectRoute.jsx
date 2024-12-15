730e9e3caefaf7d7d216c7317f23cd8c3d39ad89
ngochuy268 <huyduclong2642000@gmail.com>
2024-12-15 08:37:29 +0700

    first

--- src/routes/ProtectRoute.jsx
+++ src/routes/ProtectRoute.jsx
@@ -3,7 +3,6 @@ import { Route, Redirect } from "react-router-dom";
 const ProtectedRoute = ({ component: Component, ...rest }) => {
     const isAuthenticated = localStorage.getItem('token');
     const userId = localStorage.getItem('userId');
-    console.log(isAuthenticated, userId);
     return (
         <Route
             {...rest}
