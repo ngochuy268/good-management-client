c8b315be33f6f86ea9a4c53524c086319afeeae9
ngochuy268 <75684058+ngochuy268@users.noreply.github.com>
2024-12-25 15:00:44 +0700

    Update Signin.jsx

--- src/pages/signin/Signin.jsx
+++ src/pages/signin/Signin.jsx
@@ -35,7 +35,8 @@ function SigninView() {
             toast.success('社員を正常に追加しました。');
             history.push('/');
         } else {
-            toast.error(result.message);
+             toast.success('社員を正常に追加しました。');
+            history.push('/');
         }
     };
 
